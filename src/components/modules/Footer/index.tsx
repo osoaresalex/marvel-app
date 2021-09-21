5ad08513967386d3ba4384e757cba45a621c347b
Alex <multiwebagencia@gmail.com>
2021-09-20 23:12:04 -0300

    sobe

--- src/components/modules/Footer/index.tsx
+++ src/components/modules/Footer/index.tsx
@@ -1,19 +1,10 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { Container, Content } from './styles';
 
 export default function Footer() {
   return (
     <Container>
       <Content>
-        <div>
-          <a href="https://www.linkedin.com/in/osoaresalex/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin size={40} />
-          </a>
-          <a href="https://github.com/osoaresalex" target="_blank" rel="noopener noreferrer">
-            <FaGithub size={40} />
-          </a>
-        </div>
-        <p>Desenvolvido por Alex Soares</p>
+        <p>Alex Soares</p>
       </Content>
     </Container>
   );
